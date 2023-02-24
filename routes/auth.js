fb845b8c0f41ab30a873c54a4f327363541c9c85
RichieRich2020clear <abhijeetkamble9cd@gmail.com>
2023-02-24 09:39:28 +0530

    saa

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,19 @@
-const express = require("express");
+const express = require('express');
 const {
   register,
   login,
   getLoggedInUser,
   signinWithGitub,
   getAllUser,
-} = require("../controllers/auth");
-const authMiddleware = require("../middlewares/auth");
+} = require('../controllers/auth');
+const authMiddleware = require('../middlewares/auth');
 
 const authRouter = express.Router();
 
-authRouter.post("/register", register);
-authRouter.post("/login", login);
-authRouter.get("/", getAllUser);
-// authRouter.get('/github-signin/:code', signinWithGitub);
-authRouter.get("/loggedInUser", authMiddleware, getLoggedInUser);
+authRouter.post('/register', register);
+authRouter.post('/login', login);
+authRouter.get('/', getAllUser);
+// authRouter.get('/github-signin/:code', signi
+authRouter.get('/loggedInUser', authMiddleware, getLoggedInUser);
 
 module.exports = authRouter;
